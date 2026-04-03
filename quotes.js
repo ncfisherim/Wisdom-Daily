daded752215829357ac1ce07816704bf35d8995c
ncfisherim <ncfisherim@outlook.com>
2026-04-03 12:36:00 -0400

    add quote display logic

---
+++ quotes.js
@@ -0,0 +1,20 @@
+const quotePairs = [
+  {
+    theme: "impermanence",
+    quotes: [
+      {
+        text: "You have power over your mind, not outside events. Realize this and you will find strength.",
+        author: "Marcus Aurelius",
+        tradition: "Stoicism",
+        year: "~170 AD"
+      },
+      {
+        text: "Nothing is permanent. Don't stress yourself too much because no matter how bad the situation is, it will change.",
+        author: "Gautama Buddha",
+        tradition: "Buddhism",
+        year: "~500 BC"
+      }
+    ],
+    reflection: "Where in your life are you trying to control something outside yourself?"
+  }
+];
